ad00eb0fda98fab0f7844be0322f4307d5d29066
Albert De La Fuente Vigliotti <albert@haevas.com>
2017-12-23 01:24:18 -0200

    Remove swap

--- cryptlvm/03-lvm.sh
+++ cryptlvm/03-lvm.sh
@@ -11,5 +11,5 @@ echo -e "\nCreating logical volumes...\n$HR"
 pvcreate $INSTALL_DEV
 vgcreate storage $INSTALL_DEV
 lvcreate --size $PART_ROOT_SIZE storage --name root
-lvcreate --size $PART_SWAP_SIZE storage --name swap
+#lvcreate --size $PART_SWAP_SIZE storage --name swap
 lvcreate -l +100%FREE storage --name home
--- cryptlvm/04-format.sh
+++ cryptlvm/04-format.sh
@@ -11,5 +11,5 @@ echo -e "\nFormating partitions...\n$HR"
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4 /dev/mapper/storage-root
 mkfs.ext4 /dev/mapper/storage-home
-mkswap -L swap /dev/mapper/storage-swap
-swapon -d -L swap
+#mkswap -L swap /dev/mapper/storage-swap
+#swapon -d -L swap
